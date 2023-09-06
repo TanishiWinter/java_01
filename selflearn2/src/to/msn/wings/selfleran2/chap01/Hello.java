70549728ee237f1b48aa63c90b2e84d507dd3dfe
USER <USER@192.168.0.11>
2023-09-06 14:09:26 +0900

    first commit

---
+++ selflearn2/src/to/msn/wings/selfleran2/chap01/Hello.java
@@ -0,0 +1,14 @@
+package to.msn.wings.selfleran2.chap01;
+
+ import java.time.LocalDateTime;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		LocalDateTime time = LocalDateTime.now();
+		System.out.println(time);
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
