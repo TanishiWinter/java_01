c8617de8702d3658ec061602044657a0327c62cc
TanishiWinter <tundu3980@gmail.com>
2023-09-21 09:39:39 +0900

    ・乗算練習

--- selflearn2/src/to/msn/wings/selfleran2/chap01/Lesson2.java
+++ selflearn2/src/to/msn/wings/selfleran2/chap01/Lesson2.java
@@ -6,6 +6,20 @@ public class Lesson2 {
 		// TODO 自動生成されたメソッド・スタブ
 		//エスケープシーケンスを用いた例
 		System.out.println("私の好きな記号は二重引用符(\")です");
+		
+		//乗算練習
+		int a;
+		int b;
+		int c;
+		int d;
+		a = 100;
+		b = 2;
+		a++;
+		c = a/b;
+		d = a%b;
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(a + "を" + b + "で割った余りは" + d);
 
 	}
 
