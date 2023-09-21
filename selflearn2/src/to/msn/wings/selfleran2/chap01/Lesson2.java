da02b5b56905d12745300bab9f1b9dcc9d2c391f
TanishiWinter <tundu3980@gmail.com>
2023-09-21 09:27:03 +0900

    ・エスケープシーケンスを用いた文字表示

---
+++ selflearn2/src/to/msn/wings/selfleran2/chap01/Lesson2.java
@@ -0,0 +1,12 @@
+package to.msn.wings.selfleran2.chap01;
+
+public class Lesson2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//エスケープシーケンスを用いた例
+		System.out.println("私の好きな記号は二重引用符(\")です");
+
+	}
+
+}
