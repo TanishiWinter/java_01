e65ee72dec478f8e7a301affdef35e5115c8a338
TanishiWinter <tundu3980@gmail.com>
2023-09-24 16:35:17 +0900

    ・条件式

---
+++ selflearn2/src/to/msn/wings/selfleran2/chap01/Lesson3.java
@@ -0,0 +1,25 @@
+package to.msn.wings.selfleran2.chap01;
+
+public class Lesson3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean tenki = false;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+		}
+		
+		String name = "太郎";
+		if (name.equals("太郎")) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+		}
+
+	}
+
+}
