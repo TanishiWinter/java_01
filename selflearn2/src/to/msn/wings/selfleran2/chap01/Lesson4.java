889f76ab98af06cea4f551cbb0047586c78bae0a
TanishiWinter <tundu3980@gmail.com>
2023-09-27 20:30:44 +0900

    ・1～10までの偶数の合計と個数の合計

--- selflearn2/src/to/msn/wings/selfleran2/chap01/Lesson4.java
+++ selflearn2/src/to/msn/wings/selfleran2/chap01/Lesson4.java
@@ -38,6 +38,18 @@ public class Lesson4 {
 				System.out.println(i + "は奇数");
 			}
 		}
+		
+		
+		int sum = 0;
+		int cnt = 0;
+		for (int i = 1; i < 11; i++) {
+			if(i % 2 == 0) {
+				sum += i;
+				cnt++;
+			}
+		}
+		System.out.println("1～10までの偶数の合計値は" + sum + "です");
+		System.out.println("1～10までの偶数の個数は" + cnt + "です");
 
 	}
 
