be88bf49d9d2beb4aece789f1ea4cafb7c78d42c
TanishiWinter <tundu3980@gmail.com>
2023-10-13 22:45:27 +0900

    ・コメント追加

--- selflearn2/src/to/msn/wings/selfleran2/chap01/Another_ansor.java
+++ selflearn2/src/to/msn/wings/selfleran2/chap01/Another_ansor.java
@@ -5,7 +5,7 @@ public class Another_ansor {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		//Booleanを使用し、場合分けを実施
+		//Booleanを使用し、場合分けを実施(テスト問題)
 		//三問目
 		Boolean flg = true;
 				for(int i = 1; i < 11; i++) {
