9e30b077617d9c2cf8bad9c0982b246fd43077ce
TanishiWinter <tundu3980@gmail.com>
2023-10-15 20:35:48 +0900

    ・復習

---
+++ selflearn2/src/to/msn/wings/selfleran2/chap01/Hukusyuu.java
@@ -0,0 +1,27 @@
+package to.msn.wings.selfleran2.chap01;
+
+public class Hukusyuu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		for(int i = 10; i > 1; i--) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+		
+		int sum = 0;
+		int cnt = 0;
+		
+		for(int i = 1; i < 11; i++) {
+			if(i%2==0) {
+				sum += i;
+				cnt++ ;
+			}
+		}
+		System.out.println("合計値は"+sum+"、個数は"+cnt);
+
+	}
+
+}
