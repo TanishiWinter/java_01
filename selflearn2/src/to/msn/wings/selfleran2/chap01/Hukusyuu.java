7529e1b4ab55c06b6803eaf81a04814c188f45fd
TanishiWinter <tundu3980@gmail.com>
2023-10-22 23:47:19 +0900

    ・復習した

--- selflearn2/src/to/msn/wings/selfleran2/chap01/Hukusyuu.java
+++ selflearn2/src/to/msn/wings/selfleran2/chap01/Hukusyuu.java
@@ -5,7 +5,7 @@ public class Hukusyuu {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		//復習
+		//復習しました
 		for(int i = 10; i > 1; i--) {
 			if(i%2==0) {
 				System.out.println(i);
